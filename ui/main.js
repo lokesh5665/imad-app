722cb3362d205a4c569e542db46164a586658d95
lokesh5665 <30732403+lokesh5665@users.noreply.github.com>
2017-08-09 19:36:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
-console.log('Loaded!');
+var button=document.getElementByID('counter');
+counter=0;
+button.onclick=function()
+{
+    counter=counter+1;
+    var span=document.getElementByID('count');
+    span.innerHTML=counter.toString();
+};
