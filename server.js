a85ca92f21e90ef2b12dad4fdfa8205e1bc75aa8
lokesh5665 <30732403+lokesh5665@users.noreply.github.com>
2017-08-09 19:17:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('counter', function (req, res) {
+    counter=count+1;
+  res.send(counter.toString());
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
